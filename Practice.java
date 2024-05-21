fdb3529b86946b4c304856f63ccfa8b05193b7a3
next75 <developer.help.905@gmail.com>
2024-05-21 08:52:33 +0530

    sum of digits of given number

--- Practice.java
+++ Practice.java
@@ -1,5 +1,16 @@
+// sum of digits of given number
+
+import java.util.Scanner;
 public class Practice {
-    public static void main(String[] args){
-        System.out.println("Hello World2");
+  public static void main(String[] args) {
+    System.out.println("please start");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    int sum = 0;
+    for(int i=0 ; i<num ; ){
+    sum += num % 10;
+    num /=  10;
     }
+    System.out.println("sum is :" + sum);
+  }
 }
